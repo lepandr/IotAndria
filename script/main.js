caa884080c3987f331a795ea8c42d396d940c83b
Michele Ruta <michele.ruta@youco.eu>
2018-03-06 11:18:04 +0100

    Importazione librerie

---
+++ script/main.js
@@ -0,0 +1,4 @@
+angular.module('IotAndriaApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap','ui.bootstrap.tpls']);
+angular.module('IotAndriaApp').controller('IotAndriaAppCtrl', function ($scope) {
+
+});
